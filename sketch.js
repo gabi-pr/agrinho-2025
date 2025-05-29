4485e62b8f87c18d08db2f94eb08dbb1e5e31f29
gabi-pr <o.gabrielly13@escola.pr.gov.br>
2025-05-29 11:22:40 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+xBolinha= 300;
+yBolinha= 200;
+diametro= 20;
+velocidadeBolinha= 6;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(33);
+  actorBolinha();
+  fill("green")
 }
+function actorBolinha()
+
+{
+  circle(xBolinha, yBolinha, diametro);
+  
+   xBolinha += velocidadeBolinha;
+   yBolinha += velocidadeBolinha;
+    if(xBolinha > width || xBolinha > 0) 
+     {
+       velocidadeBolinha *= 1;
+     }
+     if(yBolinha > height || yBolinha > 0)
+{
+     velocidadeBolinha *= 1;
+}
+     

9eb2ae826d00ce331d1b33f8e278e8c046065dd1
gabi-pr <o.gabrielly13@escola.pr.gov.br>
2025-06-05 10:49:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,43 @@
-let item;
+ let item;
 let score = 0;
 let gameOver = false;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(300, 300);
   novoItem();
 }
 
 function draw() {
-  background(200);
+  background(100);
 
   // Divisão campo / cidade
   fill(100, 200, 100);
+  fill("green");
   rect(0, 0, width / 2, height);
   fill(150);
+  fill("#054F77")
   rect(width / 2, 0, width / 2, height);
-  fill("green");
+
   // Textos
   fill(0);
+  fill("#FFFDD0")
   textSize(16);
   textAlign(CENTER);
-  text("🌾 Campo", width / 4, 20);
-  text("🏙️ Cidade", 3 * width / 4, 20);
+  text("🌾 Campo", width / 5, 20);
+  text("🏙️ Cidade", 3 * width / 5, 20);
   text("Pontuação: " + score, width / 2, height - 20);
 
   if (gameOver) {
     textSize(24);
-    text("Fim de jogo!", width / 2, height / 2);
+    text("Fim!", width / 2, height / 2);
     textSize(16);
-    text("Pressione R para reiniciar", width / 2, height / 2 + 30);
+    text("T para tentar de novo", width / 2, height / 2 + 30);
     return;
   }
 
   // Item
   fill(255);
+  fill("#555D50")
   textSize(24);
   text(item.emoji, item.x, item.y);
   item.y += 2;
@@ -45,13 +49,13 @@ function draw() {
 }
 
 function keyPressed() {
-  if (gameOver && key === 'r') {
+  if (gameOver && key === 't') {
     score = 0;
     gameOver = false;
     novoItem();
   }
 
-  if (!gameOver && (key === 'a' || key === 'd')) {
+  if (!gameOver && (key === 'l' || key === 'a')) {
     if (
       (key === 'l' && item.type === 'campo') ||
       (key === 'a' && item.type === 'cidade')
@@ -77,4 +81,5 @@ function novoItem() {
   item.x = random(100, 300);
   item.y = 0;
 }
+
 
